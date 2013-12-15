ed2f355ab0d3a7c9eca1be4c5edfeb1b9bc254aa
Sébastien Cunin <git@thenturnit.net>
2013-12-15 11:14:17 +0100

    Tutorial Data Types

--- js/main.js
+++ js/main.js
@@ -1,6 +1,63 @@
 (function() {
-  var toto;
+  var greeting, isGoing, job, multi, name, num, number, obj, one, range, three, two, _i, _ref, _results;
 
-  toto = "hello ";
+  name = "value";
+
+  multi = "Hello world\n" + name + "\n";
+
+  greeting = "Hello " + name;
+
+  number = 123.456;
+
+  isGoing = false;
+
+  isGoing = true;
+
+  isGoing = true;
+
+  isGoing = false;
+
+  isGoing = false;
+
+  isGoing = true;
+
+  range = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9];
+
+  range = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+
+  range = (function() {
+    _results = [];
+    for (_i = 50; _i >= 10; _i--){ _results.push(_i); }
+    return _results;
+  }).apply(this);
+
+  obj = {
+    name: "Sébastien1",
+    job: "Programmer1"
+  };
+
+  obj = {
+    name: "Sébastien2",
+    job: "Programmer2"
+  };
+
+  name = "Sébastien 3";
+
+  job = "Programmer 3";
+
+  obj = {
+    name: name,
+    job: job
+  };
+
+  console.log(obj);
+
+  _ref = ["1", "2", "3"], one = _ref[0], two = _ref[1], three = _ref[2];
+
+  console.log("one equal " + one);
+
+  num = 10;
+
+  console.log(num + 1);
 
 }).call(this);
--- sources/data_types.coffee
+++ sources/data_types.coffee
@@ -1,1 +1,63 @@
-toto = "hello "
+name = "value"
+
+
+multi= """
+ Hello world
+ #{name}
+
+"""
+
+# String
+greeting = "Hello #{name}"
+
+
+# Number
+number= 123.456
+
+# Boolean
+isGoing = no 
+isGoing = yes
+isGoing = true
+isGoing = false
+isGoing = off
+isGoing = on
+
+
+# range
+range = [0...10]
+range = [0..10]
+range = [50..10]
+
+
+# object
+
+obj = 
+	name : "Sébastien1"
+	job  : "Programmer1"
+
+
+obj = name:"Sébastien2", job:"Programmer2"
+
+
+name =  "Sébastien 3"
+job = "Programmer 3"
+
+obj =  {name, job}
+console.log obj
+
+# Arrays
+[one,two,three] =  ["1","2","3"]
+
+console.log "one equal #{one}"
+
+
+num = 10
+console.log num + 1
+
+
+
+
+
+
+
+
