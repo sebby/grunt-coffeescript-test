97a21abe5edb745872f2bc98cb4c0c63c23799ce
Sébastien Cunin <git@thenturnit.net>
2013-12-18 06:02:17 +0100

    episode Control Structures

--- js/main.js
+++ js/main.js
@@ -1,4 +1,99 @@
 (function() {
+  var givework, greet, machine, name, person, person2;
 
+  name = "Jeremy";
+
+  if (name === "Jeremy") {
+    console.log(name);
+  } else if (name === "jim") {
+    console.log(name);
+  } else {
+    console.log("Hi!");
+  }
+
+  /*
+  Single line
+  */
+
+
+  if (name === "Jeremy") {
+    console.log(name);
+  }
+
+  machine = {
+    running: false,
+    turnOn: function() {
+      return this.running = true;
+    },
+    turnOff: function() {
+      return this.running = false;
+    }
+  };
+
+  console.log("running " + machine.running);
+
+  if (!machine.running) {
+    machine.turnOn();
+  }
+
+  if (!machine.running) {
+    machine.turnOn();
+  }
+
+  console.log("running " + machine.running);
+
+  if (!machine.running) {
+    machine.turnOn();
+  }
+
+  console.log("unless running " + machine.running);
+
+  person = {
+    name: "Seb",
+    job: "Programmer",
+    relationship: "Friend"
+  };
+
+  person2 = {
+    name: "Seb#2",
+    job: "Designer",
+    relationship: "Boss"
+  };
+
+  givework = function(person) {
+    switch (person.job) {
+      case "Programmer":
+        return console.log("You're programmer");
+      case "Designer":
+        return console.log("DDDesigner");
+      default:
+        return console.log("else");
+    }
+  };
+
+  givework(person);
+
+  givework(person2);
+
+  greet = function(person) {
+    var msg, msg2;
+    msg = person.relationship === "Friend" ? "Hi Friend!!" : person.relationship === "Boss" ? "Hi Sir!" : void 0;
+    console.log(msg);
+    msg2 = (function() {
+      switch (person.relationship) {
+        case "Friend":
+          return "Hi Friend!";
+        case "Boss":
+          return "Hi Sir";
+        default:
+          return "Hello!";
+      }
+    })();
+    return console.log(msg2);
+  };
+
+  greet(person);
+
+  greet(person2);
 
 }).call(this);
---
+++ sources/file.coffee
@@ -0,0 +1,81 @@
+name = "Jeremy"
+
+
+if name is "Jeremy" then console.log name
+else if name is "jim" then console.log name
+else console.log "Hi!"
+
+###
+Single line
+###
+console.log name if name is "Jeremy"
+
+
+machine =
+    running: no
+    turnOn: -> this.running = true
+    turnOff: -> this.running = false
+
+console.log "running #{machine.running}"
+# Solution 1
+if not machine.running
+    machine.turnOn()
+
+# Solution 2
+machine.turnOn() if not machine.running
+console.log "running #{machine.running}"
+
+#Soluton 3
+machine.turnOn() unless machine.running
+console.log "unless running #{machine.running}"
+
+
+
+# SWITCH WHEN
+
+person  = 
+    name:"Seb"
+    job:"Programmer"
+    relationship:"Friend"
+
+person2  = 
+    name : "Seb#2"
+    job : "Designer"
+    relationship:"Boss"
+
+givework = (person)->
+    switch person.job
+        when "Programmer"
+            console.log "You're programmer"
+        when "Designer"
+            console.log "DDDesigner"
+        else 
+            console.log "else"
+
+givework person
+givework person2
+
+
+greet  = (person)->
+    msg = if person.relationship is "Friend"
+         "Hi Friend!!"
+    else if person.relationship is "Boss"
+         "Hi Sir!"
+    console.log msg
+
+    msg2 = switch person.relationship
+        when "Friend" then "Hi Friend!"
+        when "Boss" then "Hi Sir"
+        else "Hello!"
+        
+    console.log msg2
+
+greet person
+greet person2
+
+
+
+
+
+
+
